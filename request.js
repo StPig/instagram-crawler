15891f85726880eed33345fb6a6082d7f3edbe52
StPig <hswu3838@gmail.com>
2018-12-29 18:28:22 +0800

    Add files via upload
    init.

---
+++ request.js
@@ -0,0 +1,42 @@
+const request = require('request');
+const fs = require('fs');
+
+const send = (url, headers) => {
+    const options = {
+        url: url,
+        headers: headers
+    };
+    return new Promise(function (resolve, reject) {
+        request(options, function (error, res, body) {
+            if (!error && res.statusCode == 200) {
+              resolve(body);
+            } else {
+              reject(error);
+            }
+        });
+    });
+};
+
+const page = (shortcode, url, headers) => {
+    const options = {
+        url: url,
+        headers: headers
+    };
+    request(options, function (error, res, body) {
+        if (!error && res.statusCode == 200) {
+            body = JSON.parse(body);
+            const msg = `shortcode: ${shortcode} like: ${body.data.shortcode_media.edge_media_preview_like.count} text: ${body.data.shortcode_media.edge_media_to_caption.edges[0].node.text}
+                ===========================================================================
+            `;
+            fs.appendFile('log.txt', msg, function (err) {
+                if (err)
+                    console.log(`[Append File Error]${err}`);
+            });
+        } else {
+            console.log(`[Requert ERROR]${error}`);
+        }
+    });
+};
+
+exports.send = send;
+exports.page = page;
